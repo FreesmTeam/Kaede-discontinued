62847e2845cdd2149f644bd468ae8db5fb7b8740
notwindstone <notwindstone@gmail.com>
2025-03-21 19:15:26 +0300

    feat: save code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import App from './App';
+import WindowHeader from './components/WindowHeader/WindowHeader';
 
 const container = document.getElementById('root');
+const windowHeader = document.getElementById('windowHeader');
 
 const root = createRoot(container!);
+const headerRoot = createRoot(windowHeader!);
 
 root.render(
     <React.StrictMode>
-        <App/>
+        <App />
+    </React.StrictMode>,
+);
+
+headerRoot.render(
+    <React.StrictMode>
+        <WindowHeader />
     </React.StrictMode>,
 );
