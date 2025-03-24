d87385aa4876b1cb69525ed89b07c125b9b3c503
notwindstone <notwindstone@gmail.com>
2025-03-24 21:55:25 +0300

    feat: remove detect-zoom and disable pinch zoom

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -24,6 +25,9 @@ func main() {
 		Frameless: true,
 		MinWidth:  400,
 		MinHeight: 400,
+		Windows: &windows.Options{
+		    DisablePinchZoom: true,
+		},
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
