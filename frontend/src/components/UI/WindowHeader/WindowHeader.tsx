5e9e13d7b48d1d75d98eb43ac2d5fa5279b285a4
notwindstone <notwindstone@gmail.com>
2025-03-24 23:11:31 +0300

    feat: implement back and forward in url history navigation

--- frontend/src/components/UI/WindowHeader/WindowHeader.tsx
+++ frontend/src/components/UI/WindowHeader/WindowHeader.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Close as windowClose, Minimise as windowMinimise, ToggleMaximise as windowToggleMaximise } from "../../../../wailsjs/go/main/App";
-import { ChevronsLeftRight, ChevronsRightLeft, Minus, X } from 'lucide-react';
+import {ArrowLeft, ArrowRight, ChevronsLeftRight, ChevronsRightLeft, Minus, X} from 'lucide-react';
 
 export default function WindowHeader() {
     const [maximised, setMaximised] = useState<boolean | null>(null);
@@ -18,10 +18,39 @@ export default function WindowHeader() {
             setMaximised(isMaximised);
         });
     }
+
+    function redirectBack() {
+        window.history.back();
+    }
+
+    function redirectForward() {
+        window.history.forward();
+    }
     
     return (
         <div className="flex flex-nowrap justify-between items-center gap-2 w-full h-8 bg-[#101013]">
-            <div className="w-24 bg-amber-900 h-full" onClick={() => window.history.back()} />
+            <div className="w-24 h-full flex flex-nowrap gap-0">
+                <button
+                    onClick={redirectBack}
+                    className="__global-no-drag group flex justify-center items-center transition cursor-pointer w-8"
+                >
+                    <div
+                        className="flex justify-center items-center transition rounded-full text-white w-6 h-6 bg-[#202026] group-hover:bg-[#303039]"
+                    >
+                        <ArrowLeft size={16} />
+                    </div>
+                </button>
+                <button
+                    onClick={redirectForward}
+                    className="__global-no-drag group flex justify-center items-center transition cursor-pointer w-8"
+                >
+                    <div
+                        className="flex justify-center items-center transition rounded-full text-white w-6 h-6 bg-[#202026] group-hover:bg-[#303039]"
+                    >
+                        <ArrowRight size={16} />
+                    </div>
+                </button>
+            </div>
             <div
                 className="select-none px-2 flex flex-nowrap justify-start gap-2 items-center w-fit">
                 <div className="font-semibold text-nowrap text-sm text-white">
