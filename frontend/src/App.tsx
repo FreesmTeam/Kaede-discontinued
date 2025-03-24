9cafe411b2634af1672cf89c317f46db39d675ea
notwindstone <notwindstone@gmail.com>
2025-03-24 23:01:31 +0300

    feat!: implement routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Greet } from "../wailsjs/go/main/App";
 import Sidebar from "./components/UI/Sidebar/Sidebar";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/page";
+import AccountPage from "./pages/account/page";
 
 function App() {
     const [resultText, setResultText] = useState("Please, click the button");
@@ -11,26 +14,33 @@ function App() {
     }
 
     return (
-        <div id="App" className="text-white flex h-[calc(100vh-32px)]">
-            <Sidebar />
-            <div className="border-t-[1px] border-l-[1px] border-[#303030] bg-black rounded-tl-md overflow-auto w-full [&::-webkit-scrollbar]:w-3 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-track]:my-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-[#eb6f92]">
-                <div id="result" className="result text-rose-900">
-                    {resultText}
+        <HashRouter basename={"/"}>
+            <div id="App" className="text-white flex h-[calc(100vh-32px)]">
+                <Sidebar/>
+                <div
+                    className="border-t-[1px] border-l-[1px] border-[#303030] bg-black rounded-tl-md overflow-auto w-full [&::-webkit-scrollbar]:w-3 [&::-webkit-scrollbar-track]:rounded-full [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-track]:my-1 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-[#eb6f92]">
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/account" element={<AccountPage/>}/>
+                    </Routes>
+                    <div id="result" className="result text-rose-900">
+                        {resultText}
+                    </div>
+                    <button className="bg-amber-900 text-white" onClick={greet}>
+                        Greet
+                    </button>
+                    {
+                        Array.from({length: 100}).map((_, index) => {
+                            return (
+                                <div key={index}>
+                                    lorem ipsum
+                                </div>
+                            );
+                        })
+                    }
                 </div>
-                <button className="bg-amber-900 text-white" onClick={greet}>
-                    Greet
-                </button>
-                {
-                    Array.from({ length: 100 }).map((_, index) => {
-                        return (
-                            <div key={index}>
-                                lorem ipsum
-                            </div>
-                        );
-                    })
-                }
             </div>
-        </div>
+        </HashRouter>
     );
 }
 
---
+++ frontend/src/pages/account/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AccountPage() {
+    return (
+        <div className="z-20 h-32 w-24 bg-amber-800 text-white">
+            welcome to account page
+        </div>
+    );
+}
