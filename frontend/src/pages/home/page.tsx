eb7d9c400fedc1aac65fafea5173eec80b13b9af
notwindstone <notwindstone@gmail.com>
2025-03-25 16:43:06 +0300

    fix: fix eslint & typescript errors

--- frontend/src/pages/home/page.tsx
+++ frontend/src/pages/home/page.tsx
@@ -2,43 +2,46 @@ import React, { useState } from "react";
 import { LaunchMinecraft } from "../../../wailsjs/go/main/App";
 
 export default function HomePage() {
-  const [launched, setLaunched] = useState(false);
-  const [selectedFile, setSelectedFile] = useState<string>("");
-  const [info, setInfo] = useState<string | null>(null);
+    const [launched, setLaunched] = useState(false);
+    const [selectedFile, setSelectedFile] = useState<string>("");
+    const [info, setInfo] = useState<string | null>(null);
 
-  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const file = event.target.files?.[0].name || "";
-    setSelectedFile(file);
-  }
+    function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const file = event.target.files?.[0].name || "";
+        setSelectedFile(file);
+    }
 
-  function launchMinecraft() {
-    LaunchMinecraft(selectedFile).then((response) => {
-      setLaunched((prev) => !prev);
-      setInfo(response.Message);
-    });
-  }
+    function launchMinecraft() {
+        LaunchMinecraft(selectedFile).then((response: {
+            Code:    number;
+            Message: string;
+        }) => {
+            setLaunched((prev) => !prev);
+            setInfo(response.Message);
+        });
+    }
 
-  return (
-    <div>
-      <div className="text-white">welcome home</div>
-      <img
-        alt="Kita oshi no ko dance gif"
-        src="https://media.tenor.com/XF0W80UFt-0AAAAj/kita-chan-kitaikuyo.gif"
-      />
-      <input
-        type="file"
-        className="rounded-md bg-rose-800 px-2 py-2 m-1"
-        accept=".jar"
-        onChange={handleFileChange}
-      ></input>
+    return (
+        <div>
+            <div className="text-white">welcome home</div>
+            <img
+                alt="Kita oshi no ko dance gif"
+                src="https://media.tenor.com/XF0W80UFt-0AAAAj/kita-chan-kitaikuyo.gif"
+            />
+            <input
+                type="file"
+                className="rounded-md bg-rose-800 px-2 py-2 m-1"
+                accept=".jar"
+                onChange={handleFileChange}
+            ></input>
 
-      <button
-        className="bg-rose-500 py-2 px-4 rounded-md mt-4 active:bg-rose-700 transition"
-        onClick={launchMinecraft}
-      >
+            <button
+                className="bg-rose-500 py-2 px-4 rounded-md mt-4 active:bg-rose-700 transition"
+                onClick={launchMinecraft}
+            >
         LAUNCH
-      </button>
-      {launched && <div className="text-sm text-zinc-300">~ {info}</div>}
-    </div>
-  );
+            </button>
+            {launched && <div className="text-sm text-zinc-300">~ {info}</div>}
+        </div>
+    );
 }
