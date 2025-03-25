ac62f8922607203e12040fe3ad20a5497b45626c
notwindstone <notwindstone@gmail.com>
2025-03-25 21:04:05 +0300

    feat: add initial ThemeContext

---
+++ frontend/src/utils/Contexts/ThemeContext.ts
@@ -0,0 +1,21 @@
+import { createContext, Dispatch, SetStateAction } from "react";
+
+const ThemeContext = createContext<{
+    theme: {
+        theme: string;
+        opacity: {
+            inside: string;
+            outside: string;
+        };
+    };
+    setTheme: Dispatch<SetStateAction<string>>;
+}>({
+    theme: {
+        theme: "dark",
+        opacity: {
+            inside: "0.5",
+            outside: "0.5",
+        },
+    },
+    setTheme: () => {},
+});
