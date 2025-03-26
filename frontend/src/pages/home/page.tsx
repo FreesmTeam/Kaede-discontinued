6d9dca4fc609d5c585a4a06e2fa63f15fb4d9a09
notwindstone <notwindstone@gmail.com>
2025-03-26 12:00:02 +0300

    feat: implement minecraft version selector

--- frontend/src/pages/home/page.tsx
+++ frontend/src/pages/home/page.tsx
@@ -3,12 +3,17 @@ import { LaunchMinecraft, GetAvailableVersions } from "../../../wailsjs/go/main/
 import ThemeOpacityCustomizer from "../../components/UI/Theme/ThemeOpacityCustomizer/ThemeOpacityCustomizer";
 
 export default function HomePage() {
-    const [availableVersions, setAvailableVersions] = useState<Array<string>>([]);
+    const [availableVersions, setAvailableVersions] = useState<Array<string> | null>(null);
     const [launched, setLaunched] = useState(false);
     const [info, setInfo] = useState<string | null>(null);
+    const [selectedVersion, setSelectedVersion] = useState<string | null>(null);
 
     function launchMinecraft() {
-        LaunchMinecraft("1.16.5", "E:\\llauncher\\Minecraft\\game").then((response: {
+        if (!selectedVersion) {
+            return;
+        }
+
+        LaunchMinecraft(selectedVersion, "E:\\llauncher\\Minecraft\\game").then((response: {
             Code:    number;
             Message: string;
         }) => {
@@ -36,12 +41,39 @@ export default function HomePage() {
             >
                 GET LOCAL VERSIONS
             </button>
-            <button
-                className="bg-rose-500 py-2 px-4 rounded-md mt-4 active:bg-rose-700 transition"
-                onClick={launchMinecraft}
-            >
-                LAUNCH
-            </button>
+            {
+                availableVersions && (
+                    <div>
+                        {selectedVersion}
+                        <select
+                            value={selectedVersion ?? ""}
+                            onChange={(event) => {
+                                setSelectedVersion(event.target.value);
+                            }}
+                        >
+                            {
+                                availableVersions.map((version) => {
+                                    return (
+                                        <option
+                                            className="text-black"
+                                            key={version}
+                                            value={version}
+                                        >
+                                            Vanilla {version}
+                                        </option>
+                                    );
+                                })
+                            }
+                        </select>
+                        <button
+                            className="bg-rose-500 py-2 px-4 rounded-md mt-4 active:bg-rose-700 transition"
+                            onClick={launchMinecraft}
+                        >
+                            LAUNCH
+                        </button>
+                    </div>
+                )
+            }
             {
                 launched && (
                     <div className="text-sm text-zinc-300">
@@ -51,7 +83,7 @@ export default function HomePage() {
             }
             <ThemeOpacityCustomizer />
             {
-                availableVersions.map((version) => {
+                availableVersions?.map((version) => {
                     return (
                         <div key={version}>
                             {version}
