9b40d386069cd9673cb652e130e2186fdb70ce8d
notwindstone <notwindstone@gmail.com>
2025-03-26 21:45:02 +0300

    feat: use @ as src/ import path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // eslint-disable-next-line
 // @ts-ignore
+import { resolve } from 'path';
+// eslint-disable-next-line
+// @ts-ignore
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
@@ -10,4 +13,11 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            // eslint-disable-next-line
+            // @ts-ignore
+            '@': resolve(__dirname, 'src'),
+        },
+    },
 });
